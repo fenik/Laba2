d39696d10b5ee9b38b253fbb94a40b14101c6efe
fenik <101297902+fenik@users.noreply.github.com>
2022-03-16 00:45:09 +0300

    456

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ res = requests.get("http://api.openweathermap.org/data/2.5/weather",
                    params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
 data = res.json()
 print("Город:", s_city)
-print("Погодные условия:", data['weather'][0]['description'])
-print("Температура:", data['main']['temp'])
+print("Погодные условия☁:", data['weather'][0]['description'])
+print("Температура🌡:", data['main']['temp'])
 print("Минимальная температура:", data['main']['temp_min'])
 print("Максимальная температура", data['main']['temp_max'])
 print("Видимость", data['visibility'])
-print("Скорость ветра", data['wind']['speed'])
+print("Скорость ветра💨", data['wind']['speed'])
 print("____________________________")
 
 res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
@@ -19,7 +19,7 @@ res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
 data = res.json()
 print("Прогноз погоды на неделю:")
 for i in data['list']:
-    print("Дата <", i['dt_txt'], "> \r\nТемпература <",
-          '{0:+3.0f}'.format(i['main']['temp']), "> \r\nПогодные условия <",
-          i['weather'][0]['description'], ">", "Скорость ветра <", i['wind']['speed'], "> \r\nВидимость <", i['visibility'], ">")
+    print("Дата <", i['dt_txt'], "> \r\nТемпература🌡 <",
+          '{0:+3.0f}'.format(i['main']['temp']), "> \r\nПогодные условия☁ <",
+          i['weather'][0]['description'], ">", "Скорость ветра💨 <", i['wind']['speed'], "> \r\nВидимость <", i['visibility'], ">")
     print("____________________________")
